aa55e70b9153c0272fc0e48984df989a631ed0ba
Mochi0213 <1275589286@qq.com>
2024-05-01 12:11:25 -0700

    Revise the Item.swift for building SwiftData

---
+++ ShareSpaceU/DataItem.swift
@@ -0,0 +1,8 @@
+//
+//  DataItem.swift
+//  ShareSpaceU
+//
+//  Created by Kuncan Wang on 5/1/24.
+//
+
+import Foundation
--- ShareSpaceU/Item.swift
+++ ShareSpaceU/Item.swift
@@ -8,11 +8,39 @@
 import Foundation
 import SwiftData
 
+enum Layout: String {
+    case twoBOneB = "2B1B"
+    case twoBTwoB = "2B2B"
+    case threeBOneB = "3B1B"
+    case threeBTwoB = "3B2B"
+    case fourBOneB = "4B1B"
+    case fourBTwoB = "4B2B"
+    case fourBThreeB = "4B3B"
+    case fourBFourB = "4B4B"
+}
+
 @Model
-final class Item {
-    var timestamp: Date
+final class Item: Identifiable {
     
-    init(timestamp: Date) {
-        self.timestamp = timestamp
+    var id: String
+    var nickName: String
+    var gender: String
+    var idealRentalArea: String
+    var idealRentalPrice: Int
+    var idealLayout: Layout
+    var restTime: String
+    var selfDescription: String
+    var hopeRoommatesAre: String
+    
+    init(nickName: String, gender: String, idealRentalArea: String, idealRentalPrice: Int,
+         idealRentalLayOut: Layout, restTime: String, selfDescription: String, hopeRoommatesAre: String) {
+        self.id = UUID().uuidString
+        self.gender = gender
+        self.idealRentalArea = idealRentalArea
+        self.idealRentalPrice = idealRentalPrice
+        self.idealLayout = idealLayout
+        self.restTime = restTime
+        self.selfDescription = selfDescription
+        self.hopeRoommatesAre = hopeRoommatesAre
     }
 }
