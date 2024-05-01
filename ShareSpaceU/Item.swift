57b0939545de50a8d8d02c5be679e5483ef22c8c
Mochi0213 <1275589286@qq.com>
2024-05-01 16:10:55 -0700

    In the process of completing MessageView and ConversationView

--- ShareSpaceU/Item.swift
+++ ShareSpaceU/Item.swift
@@ -8,16 +8,19 @@
 import Foundation
 import SwiftData
 
-enum Layout: String {
-    case twoBOneB = "2B1B"
-    case twoBTwoB = "2B2B"
-    case threeBOneB = "3B1B"
-    case threeBTwoB = "3B2B"
-    case fourBOneB = "4B1B"
-    case fourBTwoB = "4B2B"
-    case fourBThreeB = "4B3B"
-    case fourBFourB = "4B4B"
-}
+//Currently No idea why the Xcode keep complaining the enum is not persistent
+// and how to fix it
+//enum Layout: String {
+//    case twoBOneB = "2B1B"
+//    case twoBTwoB = "2B2B"
+//    case threeBOneB = "3B1B"
+//    case threeBTwoB = "3B2B"
+//    case fourBOneB = "4B1B"
+//    case fourBTwoB = "4B2B"
+//    case fourBThreeB = "4B3B"
+//    case fourBFourB = "4B4B"
+//}
+
 
 @Model
 final class Item: Identifiable {
@@ -27,20 +30,22 @@ final class Item: Identifiable {
     var gender: String
     var idealRentalArea: String
     var idealRentalPrice: Int
-    var idealLayout: Layout
+    var idealRentalLayout: String
     var restTime: String
     var selfDescription: String
     var hopeRoommatesAre: String
     
     init(nickName: String, gender: String, idealRentalArea: String, idealRentalPrice: Int,
-         idealRentalLayOut: Layout, restTime: String, selfDescription: String, hopeRoommatesAre: String) {
+         idealRentalLayout: String, restTime: String, selfDescription: String, hopeRoommatesAre: String) {
         self.id = UUID().uuidString
+        self.nickName = nickName
         self.gender = gender
         self.idealRentalArea = idealRentalArea
         self.idealRentalPrice = idealRentalPrice
-        self.idealLayout = idealLayout
+        self.idealRentalLayout = idealRentalLayout
         self.restTime = restTime
         self.selfDescription = selfDescription
         self.hopeRoommatesAre = hopeRoommatesAre
+        
     }
 }
--- ShareSpaceU/MessageView.swift
+++ ShareSpaceU/MessageView.swift
@@ -4,18 +4,19 @@ struct Message: Identifiable {
     let id: UUID
     let senderName: String
     let messagePreview: String
-//    let avatar: String
+    let avatar: String
 }
 
 struct MessageView: View {
+    @Environment(\.modelContext) private var context
     @State private var selectedMessage: Message?
     let messages = [
-        Message(id: UUID(), senderName: "Justin", messagePreview: "Hey, how are you?"),
-        Message(id: UUID(), senderName: "David", messagePreview: "Let's meet tomorrow."),
-        Message(id: UUID(), senderName: "Kenny", messagePreview: "What's your ideal price range?"),
-        Message(id: UUID(), senderName: "Robert", messagePreview: "Really? I like your idea."),
-        Message(id: UUID(), senderName: "Adam", messagePreview: "How about have a meeting this afternoon?"),
-        Message(id: UUID(), senderName: "Lenny", messagePreview:"OK bro, I got you."),
+        Message(id: UUID(), senderName: "Justin", messagePreview: "Hey, how are you?", avatar: "JustinAvatar"),
+        Message(id: UUID(), senderName: "David", messagePreview: "Let's meet tomorrow.", avatar: "DavidAvatar"),
+        Message(id: UUID(), senderName: "Kenny", messagePreview: "What's your ideal price range?", avatar:"KennyAvatar"),
+        Message(id: UUID(), senderName: "Robert", messagePreview: "Really? I like your idea.", avatar: "RobertAvatar"),
+        Message(id: UUID(), senderName: "Adam", messagePreview: "How about have a meeting this afternoon?", avatar: "AdamAvatar"),
+        Message(id: UUID(), senderName: "Lenny", messagePreview:"OK bro, I got you.", avatar: "LennyAvatar"),
     ] // currently we just hardcoded the senders and message, but hope to have better ideas in the future
 
     var body: some View {
@@ -24,29 +25,29 @@ struct MessageView: View {
                 self.selectedMessage = message
             }) {
                 HStack {
-                    Image(systemName: "person.crop.circle")
+                    Image(message.avatar)
                         .resizable()
-                        .scaledToFit()
+                        .scaledToFill()
                         .frame(width: 40, height: 40)
-                        .foregroundColor(.black)
-                        .opacity(0.7)
                         .clipShape(Circle())
                     
                     VStack(alignment: .leading) {
                         Text(message.senderName)
                             .font(.headline)
                             .foregroundColor(.black)
-                            .opacity(0.6)
+                            .opacity(0.9)
                         Text(message.messagePreview)
                             .font(.subheadline)
                             .foregroundColor(.black)
                             .opacity(0.5)
                     }
                 }
+                .padding(.vertical, 8)
             }
         }
         .sheet(item: $selectedMessage) { message in
             ConversationView(message: message)
+            
         }
     }
 }
