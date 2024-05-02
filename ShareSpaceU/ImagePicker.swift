f0129a5c2d4c0c96a0eb9310d6958a5fffa8dc43
Matthew Gerardi <matthewgerardi@berkeley.edu>
2024-05-01 20:57:43 -0700

    Implemented photo picker

--- ShareSpaceU/ImagePicker.swift
+++ ShareSpaceU/ImagePicker.swift
@@ -5,4 +5,30 @@
 //  Created by Matthew Gerardi on 5/1/24.
 //
 
-import Foundation
+import SwiftUI
+import PhotosUI
+
+@MainActor
+class ImagePicker: ObservableObject {
+    @Published var image: Image?
+    @Published var imageSelection: PhotosPickerItem? {
+        didSet {
+            if let imageSelection {
+                Task {
+                    try await loadTransferable(from: imageSelection)
+                }
+            }
+        }
+    }
+    
+    func loadTransferable(from imageSelection: PhotosPickerItem?) async throws {
+        do {
+            if let image = try await imageSelection?.loadTransferable(type: Image.self) {
+                self.image = image
+            }
+        } catch {
+            print(error.localizedDescription)
+            image = nil
+        }
+    }
+}
--- ShareSpaceU/ProfileView.swift
+++ ShareSpaceU/ProfileView.swift
@@ -6,50 +6,87 @@
 
 import Foundation
 import SwiftUI
+import PhotosUI
 
 struct ProfileView: View {
+    @StateObject var imagePicker = ImagePicker()
+    private var frameSize: CGFloat = 222
+    // @State var nickname: String
+    // @State var age: Int
+    // @State var hope: String
+    
     var body: some View {
         VStack {
-            Circle()
-                .stroke(style: StrokeStyle(lineWidth: 2, dash: [10]))
-                .foregroundColor(.blue)
-                .frame(width: 111, height: 111)
-                .overlay(alignment: .bottomTrailing) {
-                    Image(systemName: "plus").font(Font.system(size: 30, weight: .bold)).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            VStack {
+                if let image = imagePicker.image {
+                    VStack {
+                        image
+                            .resizable()
+                            .clipShape(Circle())
+                            .frame(width: frameSize, height: frameSize)
+                        PhotosPicker(selection: $imagePicker.imageSelection) {
+                            Text("Change Profile Picture").font(Font.system(size: 15, weight: .medium))
+                        }
+                        .padding(10)
+                    }
+                } else {
+                    ZStack {
+                        Circle()
+                            .stroke(style: StrokeStyle(lineWidth: 2, dash: [10]))
+                            .foregroundColor(.blue)
+                            .frame(width: frameSize, height: frameSize)
+                            .overlay(alignment: .bottomTrailing) {
+                                PhotosPicker(selection: $imagePicker.imageSelection) {
+                                    Image(systemName: "plus").font(Font.system(size: 30, weight: .bold)).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                }
+                            }
+                        Text("Upload Profile Picture Here").font(Font.system(size: 15, weight: .medium))
+                    }
                 }
-                .overlay(alignment: .center) {
-                    Text("Upload Photo Here").font(Font.system(size: 10, weight: .medium))
-                }
+            }
+            .padding(30)
             
             VStack {
                 HStack {
                     Text("Nickname:")
+                    // TextField("Enter your nickname", text: $nickname)
+                    Spacer()
                 }
                 HStack {
                     Text("Age:")
+                    Spacer()
                 }
                 HStack {
                     Text("Gender")
+                    Spacer()
                 }
                 HStack {
                     Text("Ideal Rental Area:")
+                    Spacer()
                 }
                 HStack {
                     Text("Ideal Rental Price:")
+                    Spacer()
                 }
                 HStack {
                     Text("Ideal Layout:")
+                    Spacer()
                 }
                 HStack {
                     Text("Rest Time:")
+                    Spacer()
                 }
                 HStack {
                     Text("Self Description:")
+                    Spacer()
                 }
                 HStack {
                     Text("Hope Roommates Are:")
+                    // TextField("'Super nice and fun to be around'", text: $hope)
+                    Spacer()
                 }
             }
+            .padding(.leading, 50)
         }
     }
 }
